7a578ceecc57f01f8dc0020801977a7f13a0cb86
Sebastian Gallego <sebastian.gallego@gmail.com>
2022-12-09 16:50:32 -0300

    Main Ejercicio de Prueba

--- prueba.py
+++ prueba.py
@@ -1,7 +1,8 @@
 
 """ Numeros Primos del 1 al 100 
-
 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89 , 97
+
+Muestra el Listado y la cantidad de nros primos entre 1 y 100
 
 """
 
